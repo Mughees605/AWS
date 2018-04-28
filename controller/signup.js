fb8f9ea65d4cad31a42f69675c50336ba81fbfab
mughees605 <mughees605@gmail.com>
2018-04-28 15:19:47 +0500

    serverless completed

--- controller/signup.js
+++ controller/signup.js
@@ -1,17 +1,48 @@
-function signup(event, context){
-  let { email, password } = event.body;
-  if(email){
-    context.succeed({
-        statusCode : 0,
-        statusDesc : email
-    })
-  }
-  else{
-      context.succeed({
-          statusCode: 1,
-          statusDesc : 'no email in body'
-      })
-  }
+require('dotenv').config()
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+function signup(event, context) {
+    let { email, password } = event.body;
+
+    let { CLIENT_ID, POOL_ID } = process.env;
+
+    let PoolData = {
+        ClientId: CLIENT_ID,
+        UserPoolId: POOL_ID
+    }
+
+    let userPool = new AmazonCognitoIdentity.CognitoUserPool(PoolData);
+    
+    let attributeList = [];
+    
+    let name = {
+        Name: 'given_name',
+        Value: 'mughees'
+    };
+    let profile = {
+        Name: 'profile',
+        Value: 'customer'
+    };
+    let attributeName = new AmazonCognitoIdentity.CognitoUserAttribute(name);
+    let attributeProfile = new AmazonCognitoIdentity.CognitoUserAttribute(profile);
+
+    attributeList.push(attributeName);
+    attributeList.push(attributeProfile);
+
+    userPool.signUp(email, password, attributeList, null, function (err, result) {
+        if (err) {
+           context.succeed({
+               statusCode: 0,
+               statusDesc: err
+           })
+           return
+        }
+        cognitoUser = result.user;
+        context.succeed({
+            statusCode: 1,
+            statusDesc: cognitoUser.getUsername()
+        })
+    });
 }
 
 
