e2c3d703bc0c12f6d2a047289e5af652e43b1144
mughees605 <mughees605@gmail.com>
2018-05-01 23:38:02 +0500

    not working properly

--- controller/signup.js
+++ controller/signup.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 var AWS = require('aws-sdk');
+var { CognitoUserPool } = require("amazon-cognito-identity-js")
 
 function signup(event, context) {
     let { email, password } = event.body;
 
     let { CLIENT_ID, POOL_ID } = process.env;
 
-    let PoolData = {
-        ClientId: CLIENT_ID,
-        UserPoolId: POOL_ID
-    }
+    const poolData = {
+        UserPoolId: 'us-east-2_zxTW1Lh8f',
+        ClientId: '44m58sdc3bsfqdaqphhlgj9qar'
+    };
 
-     const userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-    
+    const userPool = new CognitoUserPool(poolData);
+
     let attributeList = [];
-    
+
     let name = {
         Name: 'given_name',
         Value: 'mughees'
@@ -23,21 +24,20 @@ function signup(event, context) {
         Name: 'profile',
         Value: 'customer'
     };
-    let attributeName = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute('name','John Smith');
-    let attributeProfile = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute('given_name','John Smith');
+    let attributeName = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute('name', 'John Smith');
+    let attributeProfile = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute('given_name', 'John Smith');
 
     attributeList.push(attributeName);
     attributeList.push(attributeProfile);
 
     userPool.signUp(email, password, attributeList, null, function (err, result) {
         if (err) {
-           context.succeed({
-               statusCode: 0,
-               statusDesc: err
-           })
-           return
+            context.succeed({
+                statusCode: 0,
+                statusDesc: err
+            })
+            return
         }
-        cognitoUser = result.user;
         context.succeed({
             statusCode: 1,
             statusDesc: cognitoUser.getUsername()
